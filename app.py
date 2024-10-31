6d9c1e8a94d4fc62e5160f2f9d3713675b88d31b
Sang Je Park <parksj2480@naver.com>
2024-10-31 15:48:57 +0900

    Lock 기능 설정

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 import yaml
 from box import Box
 from utils import load_model, load_data, random_seed, process_format_to_response, process_to_format
+import threading
 
 # Configuration
 with open('./config.yaml', 'r') as f:
@@ -28,6 +29,7 @@ kwargs = {
 
 ########## FLASK APP ##########
 app = Flask(__name__)
+lock = threading.Lock()  # 전역 잠금 객체 생성
 
 # 기본 페이지를 불러오는 라우트
 @app.route('/')
@@ -36,28 +38,37 @@ def index():
 
 @app.route('/query', methods=['POST'])
 def query():
-    http_query = request.json  # JSON 형식으로 query를 받음
-    user_input = http_query.get('qry_contents', '')
-    
-    # 기존 RAG 시스템을 이용해 답변 생성
-    QU,KE,TA,TI = query_sort(query, **kwargs) # 구체화 질문, 키워드, 테이블 유무, 시간 범위
-    docs, docs_list = execute_rag(QU,KE,TA,TI, **kwargs) # RAG 나 SQL 실행
-    output = generate_answer(QU, docs, **kwargs)
-    
-    # 답변 포맷 후처리
-    if docs_list is not None:
-        retrieval = process_to_format(docs_list, type="Retrieval")
-    else:
-        retrieval = process_to_format([docs], type="SQL")
+    # 잠금을 사용하여 동시에 하나의 요청만 처리
+    if not lock.acquire(blocking=False):
+        return Response(json.dumps({"error": "현재 앱이 사용중입니다. 잠시 후에 시도해주세요."}),
+                        content_type='application/json; charset=utf-8', status=553)
 
-    answer = process_to_format([output], type="Answer")
-    
-    outputs = process_format_to_response(retrieval, answer)
+    try:
+        http_query = request.json  # JSON 형식으로 query를 받음
+        user_input = http_query.get('qry_contents', '')
+        
+        # 기존 RAG 시스템을 이용해 답변 생성
+        QU,KE,TA,TI = query_sort(user_input, **kwargs) # 구체화 질문, 키워드, 테이블 유무, 시간 범위
+        docs, docs_list = execute_rag(QU,KE,TA,TI, **kwargs) # RAG 나 SQL 실행
+        output = generate_answer(QU, docs, **kwargs)
+        
+        # 답변 포맷 후처리
+        if docs_list is not None:
+            retrieval = process_to_format(docs_list, type="Retrieval")
+        else:
+            retrieval = process_to_format([docs], type="SQL")
 
-    # 결과를 JSON 형식으로 반환
-    response = json.dumps(outputs, ensure_ascii=False)
-    response = Response(response, content_type='application/json; charset=utf-8')
-    return response
+        answer = process_to_format([output], type="Answer")
+        
+        outputs = process_format_to_response(retrieval, answer)
+
+        # 결과를 JSON 형식으로 반환
+        response = json.dumps(outputs, ensure_ascii=False)
+        response = Response(response, content_type='application/json; charset=utf-8')
+        return response
+    
+    finally:
+        lock.release()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug = False)  # 서버 실행
