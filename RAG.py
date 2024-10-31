88d25e61de3d1cf0de9c04997b158af962b39538
Sang Je Park <parksj2480@naver.com>
2024-10-31 11:06:31 +0900

    query_sort 수정

--- RAG.py
+++ RAG.py
@@ -32,28 +32,39 @@ def query_sort(model,tokenizer,query, config):
 f'''\
 <bos><start_of_turn>user
 너는 질문의 유형을 파악하고 분류하는 역할이야. 질문에 대해 질문자의 의도를 파악하고, 내가 지시하는 대로 답변형태를 맞춰서 해줘. \
-query는 질문을 구체화 하는 거야, \
+query는 질문을 구체화 하는 거야, 그리고 만약 질문에 오타가 있다면 고쳐줘. \
+keyword는 질문의 키워드를 뽑는거야. \
 table은 질문에 대한 답을 할때 표형식 데이터가 필요한지 여부야, 현재는 매출액 관련 질문만 대응 가능하니 이때만 yes로 답해줘.\
-time은 질문에 답하기 위해 필요한 데이터의 날짜 범위야(오늘 날짜는{datetime.today().year}년 {datetime.today().month}월 {datetime.today().day}일). time은 포괄적으로 잡아줘 (예시:2024년 10월에 대한 질문은 9~11월로) \
-또한, '최근'이라는 말이 들어가면 최소 8월1일 부터 시작해줘.\
+time은 질문에 답하기 위해 필요한 데이터의 날짜 범위야(오늘 날짜는{datetime.today().year}년 {datetime.today().month}월 {datetime.today().day}일). 시간의 길이는 최소 3개월로 설정해야하고, 날짜는 1일로 설정해. (예시:2024년 10월에 대한 질문은 2024-08-01:2024-11-01) \
+또한, '최근'이라는 말이 들어가면 2024-08-01:{datetime.today().year}-{datetime.today().month}-{datetime.today().day}로 설정해줘.\
 
-내가 먼저 예시 3개를 줄게
+내가 먼저 예시를 줄게
 
-질문: 남중국해의 3월 물동량을 알려줘
+질문: 최근 일본발 베트남착 매출면에서 우리사에 기여도가 높은 화주(고객)은 어떻게 돼?
 답변:
-<query/>남중국해 3월 물동량<query>
+<query/>최근 일본발 베트남착 매출면에서 우리사에 기여도가 높은 화주(고객)은 어떻게 돼?<query>
+<keyword/>일본발 베트남착 매출 기여도 화주 고객<keyword>
 <table/>yes<table>
-<time/>2024-02-01:2024-04-31<time>
+<time/>2024-08-01:2024-{datetime.today().month}-{datetime.today().day}<time>
+
+질문: 올해 3월에 중국 시장 전망에 대해 조사했던 내용을 정리해줘
+답변:
+<query/>2024년 3월 중국시장 전망에 대한 조사내용을 알려주고 정리해줘<query>
+<keyword/>2024년 3월 중국시장 전망<keyword>
+<table/>no<table>
+<time/>2024-02-01:2024-05-01<time>
 
 질문: 부산발 인도네시아착 경쟁사 서비스 및 항차수를 알려줘
 답변:
 <query/>부산 출발 인도네시아 도착 경쟁사 서비스 및 항차수<query>
+<keyword/>부산발 인도네시아착 경쟁사 서비스 항차수<keyword>
 <table/>no<table>
 <time/>all<time>
 
-질문: 인도 남성해운의 대리점 선정 과정은 어떻게 돼?
+질문: 남성해운의 인도 대리점 선정 과정은 어떻게 돼?
 답변:
-<query/>인도 대리점 선정과정<query>
+<query/>인도 대리점 선정과정을 보기 좋게 정리해줘<query>
+<keyword/>인도 대리점 선정과정<keyword>
 <table/>no<table>
 <time/>all<time>
 
@@ -81,22 +92,25 @@ time은 질문에 답하기 위해 필요한 데이터의 날짜 범위야(오�
 
     # Regular expression to extract content between <query/> and <query>
     query_pattern = r'<query/>(.*?)<query>'
+    keyword_pattern = r'<keyword/>(.*?)<keyword>'
     table_pattern = r'<table/>(.*?)<table>'
     time_pattern = r'<time/>(.*?)<time>'
 
     QU = re.search(query_pattern, answer, re.DOTALL).group(1)
+    KE = re.search(keyword_pattern, answer, re.DOTALL).group(1)
     TA = re.search(table_pattern, answer, re.DOTALL).group(1)
     TI = re.search(time_pattern, answer, re.DOTALL).group(1)
     
     # 시간이 all 이면 전체에서 검색.
     if TI == "all":
-        TI = "1900-01-01:2030-01-01"
+        TI = "1900-01-01:2099-01-01"
+    print(beep)
     print(f'사용자 질문:{query}')
     print(beep)
-    print(f'구체화 질문:{QU}, 테이블 필요 유무:{TA}, 시간:{TI}')
+    print(f'구체화 질문: {QU}, 키워드 : {KE}, 테이블 필요 유무: {TA}, 시간: {TI}')
     print(beep)
 
-    return QU, TA, TI
+    return QU, KE, TA, TI
 
 def sort_by_time(time_bound, data):
     date_format = "%Y-%m-%d"
