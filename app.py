6ec788085ffc75e544d398dd57b87e319eb95a86
SoowanInNamsung <ale783@naver.com>
2025-02-11 13:45:52 +0900

    Feat: add the GPU setting and then testing about shm-size on docker command

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ import json
 import yaml
 from box import Box
 from utils import load_model, load_data, random_seed, process_format_to_response, process_to_format, error_format
-import threading
 # Call the Ray part
 import ray
 from ray_setup import init_ray
-from ray_utils import process_query_remote
+from ray_utils import InferenceActor
 
 # Configuration
 with open('./config.yaml', 'r') as f:
@@ -34,9 +33,11 @@ kwargs = {
 ########## Ray Dashboard 8265 port ##########
 init_ray()
 
+########## Create the Ray Actors calling only one GPU ##########
+inference_actor = InferenceActor.remote(kwargs)
+
 ########## FLASK APP ##########
 app = Flask(__name__)
-lock = threading.Lock()  # 전역 잠금 객체 생성
 content_type='application/json; charset=utf-8'
 
 # 기본 페이지를 불러오는 라우트
@@ -44,17 +45,14 @@ content_type='application/json; charset=utf-8'
 def index():
     return render_template('index.html')  # index.html을 렌더링
 
-# Query Endpoint
+# Query Endpoint 
 @app.route('/query', methods=['POST'])
 def query():
     try:
-        http_query = request.json  # 클라이언트에서 전달된 JSON 요청
-        
-        # Ray 원격 함수를 호출하여 비동기로 처리 (여러 요청 동시 처리 가능)
-        # 모델 및 기타 객체가 포함된 kwargs를 그대로 전달 (필요시 ray.put으로 공유 가능)
-        future = process_query_remote.remote(http_query, kwargs)
-        response = ray.get(future)  # 원격 함수 실행 결과를 가져옴
-        
+        http_query = request.json # JSON Request From Client
+        # call the process_query method to do async
+        future = inference_actor.process_query.remote(http_query)
+        response = ray.get(future)
         return Response(response, content_type=content_type)
     except Exception as e:
         error_resp = error_format(f"서버 처리 중 오류 발생: {str(e)}", 500)
