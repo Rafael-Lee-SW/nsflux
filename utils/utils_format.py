a4098b63be4658d4cea446f12cd4625fd2059b41
SoowanInNamsung <ale783@naver.com>
2025-04-14 18:32:03 +0900

    Feat: Add the image path

--- utils/utils_format.py
+++ utils/utils_format.py
@@ -27,6 +27,7 @@ def process_to_format(qry_contents, type):
                 "rsp_tit": f"{i+1}번째 검색데이터: {form['title']} (출처:{form['file_name']})",
                 "rsp_data": form["contents"],
                 "chunk_id": form.get("chunk_id"),
+                "file_path": form['file_path'],
             }
             tmp_format["rsp_data"].append(tmp_format_)
         return tmp_format
